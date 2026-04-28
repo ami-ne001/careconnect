df39a102fd48c5ba7dfc80aa8c130451c54a8136
ami-ne001 <ami-ne001@users.noreply.github.com>
2026-04-28 04:51:41 +0100

    feat: implement docker containerization for all 7 microservices, React frontend, MySQL, and RabbitMQ

---
+++ mysql-init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS careconnect_patient;
+CREATE DATABASE IF NOT EXISTS careconnect_lab;
+CREATE DATABASE IF NOT EXISTS careconnect_billing;
+CREATE DATABASE IF NOT EXISTS careconnect_clinical;
+CREATE DATABASE IF NOT EXISTS careconnect_auth;
+CREATE DATABASE IF NOT EXISTS careconnect_appointment;
