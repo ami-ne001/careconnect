f59df643f455b22be641262b41307a7826c245e8
ami-ne001 <ami-ne001@users.noreply.github.com>
2026-05-29 17:22:56 +0100

    fix: update converter

--- patient-service/src/main/java/com/careconnect/patientservice/messaging/RabbitMQConfig.java
+++ patient-service/src/main/java/com/careconnect/patientservice/messaging/RabbitMQConfig.java
@@ -1,7 +1,7 @@
 package com.careconnect.patientservice.messaging;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -57,7 +57,7 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
